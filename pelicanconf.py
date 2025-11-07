34883944ca05c02fd27fb5376fbcf57df9fc2b07
Patrick Dornian <patrick.dornian@gmail.com>
2025-11-06 23:08:23 -0700

    added gitignore

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,7 @@ AUTHOR = 'Patrick Dornian'
 SITENAME = 'patrickd'
 SITEURL = ""
 
+# THEME = "flex"
 PATH = "content"
 
 TIMEZONE = 'MST'
@@ -19,15 +20,14 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (
     ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
 )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("LinkedIn", "https://www.linkedin.com/in/patrick-dornian/"),
+    ("GitHub", "https://github.com/pdornian"),
+
 )
 
 DEFAULT_PAGINATION = 25
