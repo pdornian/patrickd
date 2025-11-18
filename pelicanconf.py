42bba91b131987220d6d35640373a9a420c1fcea
Patrick Dornian <patrick.dornian@gmail.com>
2025-11-18 16:02:39 -0700

    readme

--- pelicanconf.py
+++ pelicanconf.py
@@ -44,7 +44,7 @@ SOCIAL = (
 
 DEFAULT_PAGINATION = 25
 
-# DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = True
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
